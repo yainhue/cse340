7cd3228767130d87e743fd3bcc4677d5216cfc17
yainhue <huentoyain4iaa@gmail.com>
2026-05-18 22:58:55 -0300

    W03 Learning Activity: Route and Query Parameters

--- src/models/projects.js
+++ src/models/projects.js
@@ -21,4 +21,25 @@ JOIN public.organizations o
   return result.rows;
 }
 
-export { getAllProjects }  
+const getProjectsByOrganizationId = async (organizationId) => {
+  const query = `
+        SELECT
+          project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          project_date
+        FROM service_project
+        WHERE organization_id = $1
+        ORDER BY project_date;
+      `;
+
+  const queryParams = [organizationId];
+  const result = await db.query(query, queryParams);
+
+  return result.rows;
+};
+
+// Export the model functions
+export { getAllProjects, getProjectsByOrganizationId };
--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,26 @@ import express from 'express';
 
 import { showIndexPage } from './controllers/index.js';
 import { showOrganizationsPage } from './controllers/organizations.js';
+import { showOrganizationDetailsPage } from './controllers/organizations.js';
 import { showProjectsPage } from './controllers/projects.js';
 import { showCategoriesPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
 
 const router = express.Router();
 
+// 
 router.get('/', showIndexPage);
+
+// 
 router.get('/organizations', showOrganizationsPage);
+
+// Route for organization details page
+router.get('/organization/:id', showOrganizationDetailsPage);
+
+// 
 router.get('/projects', showProjectsPage);
+
+// 
 router.get('/categories', showCategoriesPage);
 
 // error handling routes
